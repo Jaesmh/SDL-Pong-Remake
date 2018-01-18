bcda71636a1140698de6353c8bea50bf43618e9c
Administrator <Administrator@LAPTOP-GP5VI7AI>
2018-01-18 16:56:00 +0000

    Texturer loading class

--- SDL Pong SideProject/LTexture.cpp
+++ SDL Pong SideProject/LTexture.cpp
@@ -6,7 +6,26 @@ LTexture::LTexture()
 {
 }
 
-
-LTexture::~LTexture()
+SDL_Texture * LTexture::LoadTexture(const std::string & file_path, SDL_Renderer * Renderer)
 {
+	SDL_Surface* TempSurface = IMG_Load(file_path.c_str());
+	SDL_Texture* NewTexture = SDL_CreateTextureFromSurface(Renderer, TempSurface);
+	Free(TempSurface);
+
+	return NewTexture;
+}
+
+void LTexture::Draw(SDL_Renderer* Renderer, SDL_Texture* tex, SDL_Rect &dst)
+{
+	SDL_RenderCopy(Renderer, tex, nullptr, &dst);
+}
+
+void LTexture::Draw(SDL_Renderer* Renderer, SDL_Rect &src, SDL_Texture* tex, SDL_Rect &dst)
+{
+	SDL_RenderCopy(Renderer, tex, &src, &dst);
+}
+
+void LTexture::Free(SDL_Surface* Surface)
+{
+	SDL_FreeSurface(Surface);
 }
--- SDL Pong SideProject/LTexture.h
+++ SDL Pong SideProject/LTexture.h
@@ -1,8 +1,16 @@
-#pragma once
+#ifndef LTEXTURE_H
+#define LTEXTURE_H
+
+#include "GameSystem.h"
+
 class LTexture
 {
 public:
 	LTexture();
-	~LTexture();
+	static SDL_Texture* LoadTexture(const std::string& file_path, SDL_Renderer* Renderer);
+	static void Draw(SDL_Renderer* Renderer, SDL_Texture* tex, SDL_Rect &dst);
+	static void Draw(SDL_Renderer* Renderer, SDL_Rect &src, SDL_Texture* tex, SDL_Rect &dst);
+	static void Free(SDL_Surface* Surface);
 };
 
+#endif //LTEXTURE_H
